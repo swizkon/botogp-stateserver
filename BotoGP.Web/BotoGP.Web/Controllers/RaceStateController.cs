468dc580ee05bbb5907228e259fb25924e9dc2f9
swizkon <jonas@jerndin.se>
2017-09-07 18:20:30 +0200

    Initial commit for the skeleton

---
+++ BotoGP.Web/BotoGP.Web/Controllers/RaceStateController.cs
@@ -0,0 +1,83 @@
+﻿using BotoGP.stateserver.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+
+namespace BotoGP.stateserver.Controllers
+{
+	[Route("api/[controller]/{raceid}")]
+	public class RaceStateController : Controller
+	{
+		private static ConcurrentDictionary<string, List<RaceState>> states
+					= new ConcurrentDictionary<string, List<RaceState>>();
+
+		// GET api/racestate/race-123
+		[HttpGet]
+		public IEnumerable<RaceState> Get()
+		{
+			var race = states.GetValueOrDefault(RaceId());
+			return race;
+		}
+
+		// GET api/racestate/race-123/rider-1
+		[HttpGet("{id}")]
+		public RaceState Get(string id)
+		{
+			var race = states.GetValueOrDefault(RaceId());
+			return race?.FirstOrDefault(x => x.RiderId == id);
+		}
+
+		// POST api/values
+		[HttpPost("{id}")]
+		public void Post(string id,
+						 [FromQuery]string key,
+						 [FromQuery]int x,
+						 [FromQuery]int y)
+		{
+			var racer = Get(id);
+			if (racer != null && racer.RiderKey == key)
+			{
+				racer.ForceX += x;
+				racer.ForceY += y;
+				racer.PosX += racer.ForceX;
+				racer.PosY += racer.ForceY;
+
+				Update(racer.RiderId, racer.RiderKey, racer);
+			}
+		}
+
+		// PUT api/racestate/race-123/rider-1
+		[HttpPut("{id}")]
+		public void Put(string id, [FromQuery]string key)
+		{
+			// Some code that resets the state 
+			var state = new RaceState { RiderId = id, RiderKey = key };
+			Update(id, key, state);
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+
+		private void Update(string id, string key, RaceState newState)
+		{
+			states.AddOrUpdate(RaceId()
+							   , new List<RaceState>() { newState }
+							, (k, list) =>
+							{
+								list.RemoveAll(x => x.RiderId == id && x.RiderKey == key);
+								list.Add(newState);
+								return list;
+							});
+		}
+
+		private string RaceId()
+		{
+			return (string)this.RouteData.Values.GetValueOrDefault("raceid");
+		}
+	}
+}
---
+++ BotoGP.Web/BotoGP.Web/Models/RaceState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace BotoGP.stateserver.Models
+{
+	public class RaceState
+	{
+		public string RiderId { get; set; }
+		public string RiderKey { get; set; }
+
+		public int ForceX { get; set; }
+		public int ForceY { get; set; }
+
+		public int PosX { get; set; }
+		public int PosY { get; set; }
+
+		//public RaceState()
+		//{
+		//}
+	}
+}
