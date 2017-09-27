74b74e30e1ebf787089c9c90c58fb551510916be
swizkon <jonas@jerndin.se>
2017-09-27 17:49:00 +0200

    New stuff...

---
+++ BotoGP.Web/BotoGP.Web/RuntimeEnvironment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BotoGP.Web
+{
+    public static class Environment
+    {
+        public static bool IsDevelopment
+        {
+            get; private set;
+        }
+
+        public static void Setup(IHostingEnvironment env)
+        {
+            IsDevelopment = env.IsDevelopment();
+        }
+
+    }
+}
